2469bfab59e354ae606982b9fbb79c153f957bc7
Lucas Melo <lucas.melo044@gmail.com>
2023-11-19 12:44:54 -0300

    feat: ensure SignUpController calls EmailValidator with correct valeu

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,11 @@
 import type { Controller } from '../protocols/controller'
 import type { HttpRequest, HttpResponse } from '../protocols/http-protocols'
+import type { EmailValidator } from '../protocols/email-validator'
 import { badRequest } from '../helpers/http-helper'
 import { MissingParamError } from '../errors/missing-params'
 export class SignUpController implements Controller {
+  constructor (private readonly emailValidator: EmailValidator) {}
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
@@ -10,5 +13,8 @@ export class SignUpController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
+
+    const { email } = httpRequest.body
+    this.emailValidator.validate(email)
   }
 }
