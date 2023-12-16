74378a4d87e3fb917a7659cb291d992036e7cebb
Lucas Melo <lucas.melo044@gmail.com>
2023-12-16 19:08:48 -0300

    feat: ensure AccountMongoRespository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,6 @@
 import { MongoClient } from 'mongodb'
+import type { Collection } from 'mongodb'
+import type { AccountModel } from '../../../../domain/models/account-model'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -9,5 +11,21 @@ export const MongoHelper = {
 
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  async insertAndFind (data: any, name: string): Promise<any> {
+    const collection = this.getCollection(name)
+    const { insertedId } = await collection.insertOne(data)
+    const result = await collection.findOne({ _id: insertedId })
+    return result
+  },
+
+  map (collection: any): AccountModel {
+    const { _id, ...accountWithoutId } = collection
+    return { id: _id.toString(), ...accountWithoutId }
   }
 }
