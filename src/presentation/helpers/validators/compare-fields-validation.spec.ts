f326b5a243533f0588e53705fe141816e7b40868
Lucas Melo <lucas.melo044@gmail.com>
2023-12-25 16:19:00 -0300

    test: ensure CompareFieldValidation not return if validate succeeds

--- src/presentation/helpers/validators/compare-fields-validation.spec.ts
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -7,4 +7,10 @@ describe('CompareFields Validation', () => {
     const error = sut.validate({ field: 'any_value', fieldToCompare: 'another_value' })
     expect(error).toBeInstanceOf(InvalidParamError)
   })
+
+  it('should not return if validate succeeds', () => {
+    const sut = new CompareFieldsValidation('field', 'fieldToCompare')
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
