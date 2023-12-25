5406800def11e8c7b0fc44e365e635083e78dc99
Lucas Melo <lucas.melo044@gmail.com>
2023-12-25 10:13:50 -0300

    test: ensure LoginController returns 400 if Validation fails

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -66,6 +66,13 @@ describe('Login Controller', () => {
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 
+  it('should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
+
   it('should return 400 if no email is provided', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({
