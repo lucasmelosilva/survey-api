22da2c01b11ec9911dcb15a27ef2e90feb9367aa
Lucas Melo <lucas.melo044@gmail.com>
2023-12-26 16:21:00 -0300

    refactor: move all sut creation to a factory method

--- src/presentation/helpers/validators/compare-fields-validation.spec.ts
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -1,15 +1,19 @@
 import { InvalidParamError } from '../../errors'
 import { CompareFieldsValidation } from './compare-fields-validation'
 
+const makeSut = (): CompareFieldsValidation => (
+  new CompareFieldsValidation('field', 'fieldToCompare')
+)
+
 describe('CompareFields Validation', () => {
   it('should return a InvalidParamsError if validate fails', () => {
-    const sut = new CompareFieldsValidation('field', 'fieldToCompare')
+    const sut = makeSut()
     const error = sut.validate({ field: 'any_value', fieldToCompare: 'another_value' })
     expect(error).toBeInstanceOf(InvalidParamError)
   })
 
   it('should not return if validate succeeds', () => {
-    const sut = new CompareFieldsValidation('field', 'fieldToCompare')
+    const sut = makeSut()
     const error = sut.validate({ field: 'any_value', fieldToCompare: 'any_value' })
     expect(error).toBeFalsy()
   })
--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,15 +1,19 @@
 import { MissingParamError } from '../../errors'
 import { RequiredFieldValidation } from './required-field-validation'
 
+const makeSut = (): RequiredFieldValidation => (
+  new RequiredFieldValidation('field')
+)
+
 describe('RequiredField Validation', () => {
   it('should return a MissingParamsError if validate fails', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({})
     expect(error).toBeInstanceOf(MissingParamError)
   })
 
   it('should not return if validate succeeds', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({ field: 'any_value' })
     expect(error).toBeFalsy()
   })
