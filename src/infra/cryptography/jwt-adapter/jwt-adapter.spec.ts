3a798fee7c26edf56d7caed3d04a598f1d5ef341
Lucas Melo <lucas.melo044@gmail.com>
2024-01-02 18:36:13 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
 jest.mock('jsonwebtoken', () => ({
-  sign (): string {
+  async sign (): Promise<string> {
     return 'any_token'
   }
 }))
@@ -19,5 +19,14 @@ describe('Jwt Adapter', () => {
     const sut = new JwtAdapter('secret')
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toEqual('any_token')
+  })
+
+  it('should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any_id')
+    await expect(promise).rejects.toThrow()
   })
 })
